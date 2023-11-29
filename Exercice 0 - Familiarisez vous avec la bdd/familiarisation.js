8959c8f97b01b0cba13f4b88ef0d222ae16d5044
Mathieu Ponal <mathieu.ponal@gmail.com>
2023-11-29 12:22:25 +0100

    Changes by tomnd

--- Exercice 0 - Familiarisez vous avec la bdd/familiarisation.js
+++ Exercice 0 - Familiarisez vous avec la bdd/familiarisation.js
@@ -1,13 +1,22 @@
 // 1 Quels sont les différents bases de données atteignable via cette connexion ? Quelle commande permet de les afficher?
+// Les différentes BDD atteignables via cette connexion sont les BDD de chaque groupe, celle nommée "exercices" et des BDD sample
+// la commande qui permet de les afficher est :
+// show dbs
 
 // 2 Allez dans la BDD "exercices". Quelle commande permet de se déplacer à une nouvelle base de données. Que se passe-t-il si cette base n'existe pas ?
+// La commande qui permet de se déplacer dans une nouvelle BDD est :
+// use <nom de la BDD>
+// Si cette base n'existe pas, il en crée une en local temporairement avec le nom spécifié
 
 // 3 Quelles sont les différentes collections présentes dans cette BDD ? Quelle commande permet de les afficher.
-
+// Les différentes collections présentes dans cette BDD sont bel20, cheeses, irons, laptops
+// La commande permettant de les afficher est :
+// show collections
 
 // 4 Quelle est la commande nécessaire pour affichez la liste des documents d'une des collections.
-
+// La commande nécessaire pour afficher la liste des documents d'une des collections est :
+// db.<tableName>.find()
 
 // Vous allez maintenant changer de BDD pour pouvoir travailler sur vos propres documents.
-// Pour cela, utilisez la commande la commande de la question 2 pour ouvrir la BDD qui porte les noms des membre de votre groupe ou le numéro de votre groupe.
+// Pour cela, utilisez la commande de la question 2 pour ouvrir la BDD qui porte les noms des membre de votre groupe ou le numéro de votre groupe.
 // Si aucune BDD ne correspond à cette description, contactez-moi :)
--- Exercice 1 - find, insert, update, remove/1. laptops.js
+++ Exercice 1 - find, insert, update, remove/1. laptops.js
@@ -1,20 +1,25 @@
 // 1 Donnez liste des ordinateurs portables avec un prix compris entre 500 et 1000 euros ou avec un disque dur de 750 Go.
+// - HP Pavilion 17-g101nd
+// - Acer Aspire F5-573G-57GD'
+
+// db.laptops.find({$or: [{"price": {$gt: 500, $lt: 1000}}, {"properties.HD": {$eq: "750GB"}}]})
 
 
 // 2 Changez Windows 10 vers Windows 8
 //db.laptops.updateMany({"properties.OS": "Windows 10"}, {$set: {"properties.OS": "Windows 8"}})
 
 // 3 Donnez le nom, le prix et le système d'exploitation des 2 ordinateurs portables les moins chers.
-
+// - Nom : HP Pavilion 17-g101nd , Prix : 534, SOS : windows 8 .
+// - Nom : Acer Aspire F5-573G-57GD , Prix : 749, SE : windows 8 .
 
 // 4 Donnez tous les ordinateurs portables avec au moins 1 avis avec une note comprise entre 4 et 4,2
 // - Acer Aspire F5-573G-57GD
 
 // db.laptops.find({"reviews.score": {$gt: 3.9, $lt:4.3}})
 
-// Donnez à tous les ordinateurs portables dotés de 2 ports USB 3_0 et prenant en charge au moins 2 langues, dont le "Dutch"
+// 5 Donnez tous les ordinateurs portables dotés de 2 ports USB 3_0 et prenant en charge au moins 2 langues, dont le "Dutch"
 
-
+// - Acer Aspire F5-573G-57GD
 
 // db.laptops.find({"properties.USB.USB3_0": 2, "LanguageOS": {$size: {$gt: 1}}, "LanguageOS": "Dutch"})
 
