6017651d8262a625bbfe1ca8a581ebdc8e099e77
Mathieu Ponal <mathieu.ponal@gmail.com>
2023-11-29 12:22:19 +0100

    le push du matin

--- Exercice 1 - find, insert, update, remove/1. laptops.js
+++ Exercice 1 - find, insert, update, remove/1. laptops.js
@@ -2,19 +2,21 @@
 
 
 // 2 Changez Windows 10 vers Windows 8
-
+//db.laptops.updateMany({"properties.OS": "Windows 10"}, {$set: {"properties.OS": "Windows 8"}})
 
 // 3 Donnez le nom, le prix et le système d'exploitation des 2 ordinateurs portables les moins chers.
 
 
-// 4 Donnez à tous les ordinateurs portables avec au moins 1 avis avec une note comprise entre 4 et 4,2
+// 4 Donnez tous les ordinateurs portables avec au moins 1 avis avec une note comprise entre 4 et 4,2
+// - Acer Aspire F5-573G-57GD
 
+// db.laptops.find({"reviews.score": {$gt: 3.9, $lt:4.3}})
 
 // Donnez à tous les ordinateurs portables dotés de 2 ports USB 3_0 et prenant en charge au moins 2 langues, dont le "Dutch"
 
 
 
-
+// db.laptops.find({"properties.USB.USB3_0": 2, "LanguageOS": {$size: {$gt: 1}}, "LanguageOS": "Dutch"})
 
 
 
